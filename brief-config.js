969500b98a8f162c9b5baea7b6a348d414985d92
lukiz <apple@macbook-air-lukiz.home>
2026-03-26 11:32:08 +0100

    Update brief Apps Script URL

--- brief-config.js
+++ brief-config.js
@@ -1,4 +1,4 @@
 window.SENNS_BRIEF_CONFIG = window.SENNS_BRIEF_CONFIG || {
   // Paste your deployed Google Apps Script Web App URL here.
-  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxS7kfQDn6aWelQmJo-k3A_RpGKscLlWz4xHF0OsYwHiTJtJepGlkTwCN3LkuNpji9N/exec'
+  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbwLoxjESZQSay0lAAoiTHZgOh1TiEwbfeR7F_42WWHEROr8YHNXz7fxO7VYLRKaapA/exec'
 };
