31e7cfffaf34209d456672716bd6a461451731b9
lukiz <apple@macbook-air-lukiz.home>
2026-03-26 12:44:53 +0100

    Update brief backend URL

--- brief-config.js
+++ brief-config.js
@@ -1,4 +1,4 @@
 window.SENNS_BRIEF_CONFIG = window.SENNS_BRIEF_CONFIG || {
   // Paste your deployed Google Apps Script Web App URL here.
-  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbwLoxjESZQSay0lAAoiTHZgOh1TiEwbfeR7F_42WWHEROr8YHNXz7fxO7VYLRKaapA/exec'
+  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbyHpcxztq4MrIPEUmw4I34-2gGp3JBn-ZX0i1AsgAFTPLq4bcvNLn4RPoeoxOhJz5Pp/exec'
 };
